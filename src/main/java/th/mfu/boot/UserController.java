f8dd291e17107d426a2927627df7402b8304fc16
Myint Thwe Cho <6731503069@lamduan.mfu.ac.th>
2025-09-01 08:46:51 +0000

    DONE

--- src/main/java/th/mfu/boot/UserController.java
+++ src/main/java/th/mfu/boot/UserController.java
@@ -10,27 +10,41 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
-    public static Map<String, User> users = new HashMap<String, User>();
+    public static HashMap<String, User> users = new HashMap<String, User>();
 
-   
-    public ResponseEntity<String> registerUser( User user) {
-      //TODO
-      return null;
+    @PostMapping
+    public ResponseEntity<String> registerUser(@RequestBody User user) {
+        String username = user.getUsername();
+
+        if (users.containsKey(username)) {
+            return new ResponseEntity<>("Username already exists", HttpStatus.CONFLICT); // 409
+        }
+
+        users.put(username, user);
+        return new ResponseEntity<>("User registered successfully", HttpStatus.OK); // 201
     }
 
+    @GetMapping
     public ResponseEntity<Collection<User>> list() {
-        //TODO
-        return null;
+        return new ResponseEntity<>(users.values(), HttpStatus.OK); // 200
     }
 
-    public ResponseEntity<User> getUser(String username) {
-        //TODO
-        return null;
+    @GetMapping("/{username}")
+    public ResponseEntity<User> getUser(@PathVariable String username) {
+        User user = users.get(username);
+
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND); // 404
+        }
+
+        return new ResponseEntity<>(user, HttpStatus.OK); // 200
     }
-    
+
 }
